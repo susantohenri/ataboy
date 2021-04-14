b62946c029ad34b27e8b6c6aaac2a59bb36f9c58
henrisusanto <henri@member.id>
2021-04-14 17:31:28 +0700

    setup PWA, change IP as you need

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-var dataCacheName = 'pwa-calculator-v1'
-var cacheName = 'pwa-calculator-v1'
-var dataUrl = 'https://henri.xsanisty.com/calculator/'
+var dataCacheName = 'ataboy-v1.0'
+var cacheName = 'ataboy-v1.0'
+var dataUrl = 'http://192.168.43.39/ataboy/'
 var PATH = dataUrl
 var filesToCache = [
    PATH + '/',
@@ -10,30 +10,24 @@ var filesToCache = [
    PATH + '/css/main.css'
 ]
 self.addEventListener('install', function(e) {
-   console.log('[ServiceWorker] Install')
    e.waitUntil(caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell')
       return cache.addAll(filesToCache)
    }))
 })
 self.addEventListener('activate', function(e) {
-   console.log('[ServiceWorker] Activate')
    e.waitUntil(caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
          if (key !== cacheName) {
-            console.log('[ServiceWorker] Removing old cache', key)
             return caches.delete(key)
          }
       }))
    }))
 })
 self.addEventListener('fetch', function(e) {
-   console.log('[ServiceWorker] Fetch', e.request.url)
    if (e.request.url.indexOf(dataUrl) === 0) {
       e.respondWith(fetch(e.request).then(function(response) {
          return caches.open(dataCacheName).then(function(cache) {
             cache.put(e.request.url, response.clone())
-            console.log('[ServiceWorker] Fetched&Cached Data')
             return response;
          })
       }))
